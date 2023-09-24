c3759c88438d156a7531ffc38d70968cfe4e027c
Anurag <anuragtiwariji2012@gmail.com>
2023-09-24 12:48:10 +0530

    Added menu driven

--- FirstCode/src/First.java
+++ FirstCode/src/First.java
@@ -1,16 +1,36 @@
 import java.util.Scanner;
 
 public class First {
-
+	
 	public static void main(String[] args) {
-		
+		Scanner scanner=new Scanner(System.in);
+		menudrivenfunction();
+		int a=scanner.nextInt();
+		switch (a) {
+		case 1:
+			add();
+			break;
+		case 2:
+			Second sc =new Second();
+			sc.sub();
+			break;
+		default:
+			System.out.println("Invalid Input !!!!");
+			break;
+		}		
+	}	
+	public static void add() {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the first number : ");
 		int a= scanner.nextInt();
 		System.out.println("Enter the second number : ");
 		int b= scanner.nextInt();
 		int c= a+b;
-		System.out.println("Sum of two numbers is : "+c);
+		System.out.println("Sum of two numbers is : "+c);		
+	}
+	private static void menudrivenfunction() {
 		
+		System.out.println("Press 1 for addition :");
+		System.out.println("Press 2 for substraction :");	
 	}
 }
--- FirstCode/src/Second.java
+++ FirstCode/src/Second.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
 public class Second {
+	
+	public void sub() {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the first number : ");
+		int a= scanner.nextInt();
+		System.out.println("Enter the second number : ");
+		int b= scanner.nextInt();
+		int c= a-b;
+		System.out.println("Sum of two numbers is : "+c);
+		
+	}
+	
 
 }
